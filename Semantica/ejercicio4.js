5b6ac2df3c3e4991aa9ea25b457f47018616519e
keylacarrion00 <kb.carrionflores@colegiocalasanz.edu.ar>
2025-10-30 14:48:46 -0300

    Delete Semantica directory

--- Semantica/ejercicio4.js
+++
@@ -1,9 +0,0 @@
-    function pedirNombre() {
-      let nombre = prompt("¿Cómo te llamas?");
-      
-      if (nombre) {
-        alert("Hola " + nombre + ", ¡bienvenido a la clase de JavaScript!");
-      } else {
-        alert("No escribiste tu nombre, inténtalo de nuevo.");
-      }
-    }
--- Semantica/ejercicio6.js
+++
@@ -1,8 +0,0 @@
-    // Referencias a los elementos
-    let boton = document.getElementById("btntexto");
-    let parrafo = document.getElementById("texto");
-
-    // Evento click para cambiar el texto
-    boton.addEventListener("click", function() {
-      parrafo.innerHTML = "Texto modificado";
-    });
